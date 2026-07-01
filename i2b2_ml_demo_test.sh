790b4acc8868a1f28282f4fa188ae3cc75933e16
root <root@DESKTOP-9RTK407.localdomain>
2026-07-01 10:59:14 +0530

    docker cmd fix

--- i2b2_ml_demo_test.sh
+++ i2b2_ml_demo_test.sh
@@ -4,4 +4,4 @@ docker volume rm -f $(docker volume ls -q)
 cd deployment/pg
 docker compose up -d i2b2-etl
 
-docker exec -it i2b2-etl bash -c "source /usr/src/app/.venv/bin/activate && sleep 10 && python -m i2b2_cdi.job.jobWatcher"
+docker exec -i i2b2-etl bash -c "source /usr/src/app/.venv/bin/activate && sleep 10 && python -m i2b2_cdi.job.jobWatcher"
